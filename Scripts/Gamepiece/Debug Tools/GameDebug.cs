bfa28bb7f72d13503d67bbca314c4aea4e13bf1f
nsgbcz <deplmgms@gmail.com>
2020-03-27 11:37:28 +0300

    Update GameDebug.cs

--- Scripts/Gamepiece/Debug Tools/GameDebug.cs
+++ Scripts/Gamepiece/Debug Tools/GameDebug.cs
@@ -2,71 +2,68 @@
 using System.Diagnostics.Tracing;
 using SD = System.Diagnostics;
 
-namespace Debugs
+public static class GameDebug
 {
-	public static class GameDebug
+	private static bool Enabled;
+	private static bool Max;
+	[RuntimeInitializeOnLoadMethod]
+	public static void Init()
 	{
-		private static bool Enabled;
-		private static bool Max;
-		[RuntimeInitializeOnLoadMethod]
-		public static void Init()
-		{
 #if UNITY_EDITOR
-			Enabled = true;
-			Max = false;
+		Enabled = true;
+		Max = false;
 #elif DEBUG
-			Enabled = true;
-			Max = true;
+		Enabled = true;
+		Max = true;
 #else
 			
-			Enabled = false;
-			Max = false;
+		Enabled = false;
+		Max = false;
 #endif
-			Debug.unityLogger.logEnabled = Enabled;
-		}
+		Debug.unityLogger.logEnabled = Enabled;
+	}
 
-		/// <param name="wrap">If true - also sends Class and Method names where you call Debug.</param>
-		public static void Log(string message, bool wrap = true)
+	/// <param name="wrap">If true - also sends Class and Method names where you call Debug.</param>
+	public static void Log(object message = null, bool wrap = true)
+	{
+		if (Enabled)
 		{
-			if (Enabled)
-			{
-				Debug.Log(wrap ? WrapMessage(message) : message);
-			}
+			Debug.Log(wrap ? WrapMessage(message) : message);
 		}
+	}
 
-		/// <param name="wrap">If true - also sends Class and Method names where you call Debug.</param>
-		public static void LogWarning(string message, bool wrap = true)
+	/// <param name="wrap">If true - also sends Class and Method names where you call Debug.</param>
+	public static void LogWarning(object message = null, bool wrap = true)
+	{
+		if (Enabled)
 		{
-			if (Enabled)
-			{
-				Debug.LogWarning(wrap ? WrapMessage(message) : message);
-			}
+			Debug.LogWarning(wrap ? WrapMessage(message) : message);
 		}
+	}
 
-		/// <param name="wrap">If true - also sends Class and Method names where you call Debug.</param>
-		public static void LogError(string message, bool wrap = true)
+	/// <param name="wrap">If true - also sends Class and Method names where you call Debug.</param>
+	public static void LogError(object message = null, bool wrap = true)
+	{
+		if (Enabled)
 		{
-			if (Enabled)
-			{
-				Debug.LogError(wrap ? WrapMessage(message) : message);
-			}
+			Debug.LogError(wrap ? WrapMessage(message) : message);
 		}
+	}
 
-		/// <param name="wrap">If true - also sends Class and Method names where you call Debug.</param>
-		public static void LogMax(string message, bool wrap = false)
+	/// <param name="wrap">If true - also sends Class and Method names where you call Debug.</param>
+	public static void LogMax(object message = null, bool wrap = false)
+	{
+		if (Max)
 		{
-			if (Max)
-			{
-				Debug.Log(wrap ? WrapMessage(message) : message);
-			}
+			Debug.Log(wrap ? WrapMessage(message) : message);
 		}
-		/// <summary>
-		/// You don't need explicitly point where you call Debug.
-		/// </summary>
-		static string WrapMessage(string message)
-		{
-			var meth = new SD.StackTrace().GetFrame(1).GetMethod();
-			return $"{meth.DeclaringType.Name}.{meth.Name}:\"{message}\"";
-		}
+	}
+	/// <summary>
+	/// You don't need explicitly point where you call Debug.
+	/// </summary>
+	static string WrapMessage(object message = null)
+	{
+		var meth = new SD.StackTrace().GetFrame(2).GetMethod();
+		return $"{meth.DeclaringType.Name}.{meth.Name}:\"{message ?? ""}\"";
 	}
 }
